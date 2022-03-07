986dcd82341439981c9cd6e1f8826651c2fb210d
Herdina <gherdina7@gmail.com>
2022-03-07 19:35:50 -0300

    exercicio 3 finalizado

---
+++ Módulo1_Fundamentos/Bloco4_JS_Lógica/Dia1_JavaScript_Primeiros_Passos/exercicio3.js
@@ -0,0 +1,12 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+}
+else if (num2 > num1 && num2 > num3) {
+  console.log(num2);
+} else {
+  console.log(num3);
+}
