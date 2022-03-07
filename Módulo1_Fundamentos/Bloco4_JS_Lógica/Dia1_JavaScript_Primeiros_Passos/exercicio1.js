2c16cf5be9e6a1ac02f97d5667e66e728be355ab
Herdina <gherdina7@gmail.com>
2022-03-07 19:26:59 -0300

    exercicio 1 finalizado

---
+++ Módulo1_Fundamentos/Bloco4_JS_Lógica/Dia1_JavaScript_Primeiros_Passos/exercicio1.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 2;
+
+let adicao = a + b;
+let subtracao = a - b;
+let divisao = a / b;
+let multiplicacao = a * b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(divisao);
+console.log(multiplicacao);
+console.log(modulo);
