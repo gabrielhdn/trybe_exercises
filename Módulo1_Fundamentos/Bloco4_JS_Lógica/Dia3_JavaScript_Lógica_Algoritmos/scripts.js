cecdfdc6d01b08c94e2c31d64e0c2c8490863f34
Herdina <gherdina7@gmail.com>
2022-03-16 15:54:08 -0300

    mudança no arquivo scripts.js - bloco4

--- Módulo1_Fundamentos/Bloco4_JS_Lógica/Dia3_JavaScript_Lógica_Algoritmos/scripts.js
+++ Módulo1_Fundamentos/Bloco4_JS_Lógica/Dia3_JavaScript_Lógica_Algoritmos/scripts.js
@@ -16,14 +16,14 @@
 
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-// let word = 'herdina';
-// let inverse = '';
+let word = 'herdina';
+let inverse = '';
 
-// for (let index = word.length - 1; index >= 0; index -= 1) {
-//   inverse += word[index];
-// }
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  inverse += word[index];
+}
 
-// console.log(inverse);
+console.log(inverse);
 
 // 3- Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
