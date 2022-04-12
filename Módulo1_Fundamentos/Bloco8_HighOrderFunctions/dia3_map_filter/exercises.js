a7063ef4c322e3e795fb661330a6b6cc66089bd5
Herdina <gherdina7@gmail.com>
2022-04-11 21:37:20 -0300

    comentario adicionado

--- Módulo1_Fundamentos/Bloco8_HighOrderFunctions/dia3_map_filter/exercises.js
+++ Módulo1_Fundamentos/Bloco8_HighOrderFunctions/dia3_map_filter/exercises.js
@@ -61,6 +61,8 @@ const books = [
   },
 ];
 
+console.log(books.sort((a, b) => a.name < b.name ? -1 : 1));
+
 // 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 // Dica: Use a função map
 
